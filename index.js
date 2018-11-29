1e81a17be40d3ea3ea7aa38ae66d97c0bb0214b3
alex papapetrou <apapa6@hotmail.com>
2018-11-29 23:32:52 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function shout(string) { return string.toUpperCase() }
+function whisper(string) { return string.toLowerCase() }
+var uppercase = "HELLO"
+uppercase.toUpperCase() === uppercase
+var lowercase = "hello"
+lowercase.toLowerCase() === lowercase
+var mixedCase = 'Hi there'
+mixedCase.toLowerCase === mixedCase
+mixedCase.toUpperCase === mixedCase
+function longShout(string) { console.log ("string".toUpperCase()) }
